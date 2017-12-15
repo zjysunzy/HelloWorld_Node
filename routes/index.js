510546600a79acc76553ac86c744b38293e3afe3
zhangjiayong <zhangjiayong@ciwong.com>
2017-12-15 15:42:47 +0800

    修改

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express111' });
+  res.render('index', { title: 'Express1112222' });
 });
 
 module.exports = router;
